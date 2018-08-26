271ae88254d361eb8ff34115fa3d62eb8ec886c7
katy@karstaway.com <katy@karstaway.com>
2018-08-26 14:30:44 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -198,4 +198,8 @@ end
 
 puts team_names
 
-
+def good_practices
+  game_hash.each do |location, team_data|
+    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+    binding.pry
+      team_data.each do |attribute, data|
